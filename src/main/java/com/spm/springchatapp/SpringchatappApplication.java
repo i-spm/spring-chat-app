7e8fa61732987dcddf0ebb1ce756bc386d85cb61
i-spm <shivspm54@gmail.com>
2020-09-26 21:21:11 +0530

    initial commit

---
+++ src/main/java/com/spm/springchatapp/SpringchatappApplication.java
@@ -0,0 +1,13 @@
+package com.spm.springchatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringchatappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringchatappApplication.class, args);
+	}
+
+}
